fc75b51e43e95e42a8138bf06b17f5afe91ad3b6
Parelho <vinicius.parelho@outlook.com>
2025-04-27 23:28:23 -0300

    tailwind

---
+++ ResearchPlanner/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="min-h-screen max-h-full bg-white">
+      <div className="font-bold text-grey-900">Testing</div>
+    </div>
+  );
+}
