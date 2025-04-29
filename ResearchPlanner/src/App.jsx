d71a4223c5170aff130827955d8888da72c4a2ac
Parelho <vinicius.parelho@outlook.com>
2025-04-29 20:01:09 -0300

    fix: app now loads into home page by default

--- ResearchPlanner/src/App.jsx
+++ ResearchPlanner/src/App.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import Navbar from "./components/Navbar/index.jsx"
 import Home from "./components/Home/index.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Researchers from "./components/Researchers/index.jsx";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className="flex h-screen overflow-hidden pr-2">
         <Navbar />
-        <div className="flex-1 overflow-y-auto">
+        <div className="flex-1 overflow-y-auto p-2">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/researchers" element={<Researchers />} />
           </Routes>
         </div>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
--- ResearchPlanner/src/components/Navbar/index.jsx
+++ ResearchPlanner/src/components/Navbar/index.jsx
@@ -5,12 +5,16 @@ import 'boxicons/css/boxicons.min.css';
 export default function Navbar() {
   return (
     <div>
-      <div className="min-h-screen flex flex-row bg-gray-100">
-        <div className="flex flex-col w-56 bg-white rounded-r-3xl overflow-hidden">
-          <div className="flex items-center justify-center h-20 shadow-md">
-            <h1 className="text-3xl uppercase text-indigo-500">Logo</h1>
-          </div>
+      <div className="min-h-screen flex flex-row bg-gray-100 border-r border-black">
+        <div className="flex flex-col w-56 bg-white overflow-hidden">
           <ul className="flex flex-col py-4">
+            <li>
+              <Link to="/" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
+                <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400">
+                </span>
+                <span className="text-3xl font-medium">Home</span>
+              </Link>
+            </li>
             <li>
               <Link to="/researchers" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
                 <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400">
