33a01cc4791f24bb8d12a3929e34e8509e6b6b7f
Parelho <vinicius.parelho@outlook.com>
2025-04-29 18:02:50 -0300

    routing fix

--- ResearchPlanner/src/App.jsx
+++ ResearchPlanner/src/App.jsx
@@ -1,8 +1,17 @@
 import React from "react";
-import Home from "./components/Home/index.jsx"
+import Navbar from "./components/Navbar/index.jsx"
+import Home from "./components/Home/index.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Researchers from "./components/Researchers/index.jsx";
 
 export default function App() {
   return (
-    <Home/>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/researchers" element={<Researchers />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
--- ResearchPlanner/src/components/Home/index.jsx
+++ ResearchPlanner/src/components/Home/index.jsx
@@ -1,43 +1,8 @@
-import React from "react";
-import 'boxicons/css/boxicons.min.css';
-
 export default function Home() {
-  return (
-    <div>
-      <link rel="stylesheet" href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" />
-      <div className="min-h-screen flex flex-row bg-gray-100">
-        <div className="flex flex-col w-56 bg-white rounded-r-3xl overflow-hidden">
-          <div className="flex items-center justify-center h-20 shadow-md">
-            <h1 className="text-3xl uppercase text-indigo-500">Logo</h1>
-          </div>
-          <ul className="flex flex-col py-4">
-            <li>
-              <a href="#" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-                <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400">
-                  <i className="bx bxs-graduation"></i>
-                </span>
-                <span className="text-sm font-medium">Pesquisadores</span>
-              </a>
-            </li>
-            <li>
-              <a href="#" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-                <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400">
-                  <i className="bx bx-user"></i>
-                </span>
-                <span className="text-sm font-medium">Perfil</span>
-              </a>
-            </li>
-            <li>
-              <a href="#" className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-                <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400">
-                  <i className="bx bx-log-out"></i>
-                </span>
-                <span className="text-sm font-medium">Logout</span>
-              </a>
-            </li>
-          </ul>
+    return (
+        <div style={{ padding: '20px' }}>
+            <h1>Home Page</h1>
+            <p>Welcome to the site</p>
         </div>
-      </div>
-    </div>
-  );
+    )
 }
